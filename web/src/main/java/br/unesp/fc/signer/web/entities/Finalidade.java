71cdc3bf987cd5444885b6292dec1e49127763c7
Demitrius Belai <demitrius.belai@unesp.br>
2025-04-10 10:22:50 -0300

    Adiciona finalidade ausente em dbeefe6

---
+++ web/src/main/java/br/unesp/fc/signer/web/entities/Finalidade.java
@@ -0,0 +1,25 @@
+package br.unesp.fc.signer.web.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.sql.Date;
+import lombok.Data;
+
+@Entity
+@Data
+public class Finalidade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private Date data;
+
+}
